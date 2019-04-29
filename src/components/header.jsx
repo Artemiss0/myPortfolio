6f017ac4c3662b43326fcf12e370d5dde2247169
Artemiss0 <nm.overmeer"gmail.com>
2019-04-29 17:09:09 +0200

    Changed the header image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import headerImage from '../images/headerIllustration.svg';
+import headerImage from '../images/headerIllustration.jpg';
 
 class About extends Component {
     render() {
