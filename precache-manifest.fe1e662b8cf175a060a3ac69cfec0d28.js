1b17e7617fc8c8df2cd8c7c2f701e7d298f7c855
Nienke Overmeer <0947037@hr.nl>
2021-01-19 17:15:37 +0100

    Updates

---
+++ precache-manifest.fe1e662b8cf175a060a3ac69cfec0d28.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "e24bc3a23d4b63cfd967",
+    "url": "/myPortfolio/static/css/main.37a45681.chunk.css"
+  },
+  {
+    "revision": "e24bc3a23d4b63cfd967",
+    "url": "/myPortfolio/static/js/main.5006ff44.chunk.js"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  },
+  {
+    "revision": "bf2d0b0d81584bb9bfdb",
+    "url": "/myPortfolio/static/css/2.cc581882.chunk.css"
+  },
+  {
+    "revision": "bf2d0b0d81584bb9bfdb",
+    "url": "/myPortfolio/static/js/2.e0719d8e.chunk.js"
+  },
+  {
+    "revision": "73f169c30ab49b9de731f9482a5c5e8d",
+    "url": "/myPortfolio/static/media/headerIllustration.73f169c3.jpg"
+  },
+  {
+    "revision": "a9db95067cd346729d105f043a561845",
+    "url": "/myPortfolio/static/media/me.a9db9506.png"
+  },
+  {
+    "revision": "aed515ccc70ff85408c30313b9efc532",
+    "url": "/myPortfolio/static/media/carrierPigeon.aed515cc.svg"
+  },
+  {
+    "revision": "17d7075815382d8ce485036a49b239f8",
+    "url": "/myPortfolio/static/media/CV_NienkeOvermeer_NL.17d70758.pdf"
+  },
+  {
+    "revision": "94249c40218faa7422428c717e779a7c",
+    "url": "/myPortfolio/static/media/dynasteaHeader.94249c40.jpg"
+  },
+  {
+    "revision": "aa283394b0c96f062216c839c3a8088b",
+    "url": "/myPortfolio/static/media/dynasteaMockupBookingSystem.aa283394.png"
+  },
+  {
+    "revision": "3b86ebe61a3784a09e73bf61f5dfd5c0",
+    "url": "/myPortfolio/static/media/dynasteaMockupWebdesign.3b86ebe6.jpg"
+  },
+  {
+    "revision": "c3702162fc97f4fc8940c7b428030e5f",
+    "url": "/myPortfolio/static/media/headerIllustration.c3702162.svg"
+  },
+  {
+    "revision": "5d422168843237e0024c0498e24de3ca",
+    "url": "/myPortfolio/static/media/hetKegelhonkHeader.5d422168.jpg"
+  },
+  {
+    "revision": "d55abc67fb9fe88c4784bbcf04a0beca",
+    "url": "/myPortfolio/static/media/hetKegelhonkMockup1.d55abc67.jpg"
+  },
+  {
+    "revision": "68801cfbe556d2e8beba99abb65922e2",
+    "url": "/myPortfolio/static/media/hetKegelhonkMockup2.68801cfb.jpg"
+  },
+  {
+    "revision": "08e5f213c73ea8b06adfcf2d2ba6ddee",
+    "url": "/myPortfolio/static/media/weCreateHeader.08e5f213.jpg"
+  },
+  {
+    "revision": "7200a6536f72763dc0cc162a3294582d",
+    "url": "/myPortfolio/static/media/wecreateMockup1.7200a653.jpg"
+  },
+  {
+    "revision": "bec6b270067086fbe05d50cb65c5fe02",
+    "url": "/myPortfolio/static/media/wecreateMockup2.bec6b270.jpg"
+  },
+  {
+    "revision": "a4ccea7dc18b6f31eba731b6e53f966a",
+    "url": "/myPortfolio/static/media/manrope-regular.a4ccea7d.woff2"
+  },
+  {
+    "revision": "12ff9ff63fd9dbe27fc440e043e5854a",
+    "url": "/myPortfolio/static/media/manrope-regular.12ff9ff6.woff"
+  },
+  {
+    "revision": "9b61c79f6c46b63010154e79061b0b49",
+    "url": "/myPortfolio/static/media/manrope-bold.9b61c79f.woff2"
+  },
+  {
+    "revision": "35815d106062352af312f66d2fa2b53c",
+    "url": "/myPortfolio/static/media/manrope-bold.35815d10.woff"
+  },
+  {
+    "revision": "046b1d96c55e5cc50fd3bec6714394a8",
+    "url": "/myPortfolio/static/media/manrope-extrabold.046b1d96.woff2"
+  },
+  {
+    "revision": "f7cabd527f0a5eba81af75148e5ab96d",
+    "url": "/myPortfolio/static/media/manrope-extrabold.f7cabd52.woff"
+  },
+  {
+    "revision": "f31260511ef927740a58953665aee37e",
+    "url": "/myPortfolio/static/media/manrope-light.f3126051.woff"
+  },
+  {
+    "revision": "031e2fb83d5675a78dc5c5c1db8029f1",
+    "url": "/myPortfolio/static/media/manrope-light.031e2fb8.woff2"
+  },
+  {
+    "revision": "09fbc89f94a198940b9076e7d9218e77",
+    "url": "/myPortfolio/index.html"
+  }
+];
